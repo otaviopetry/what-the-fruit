dcee87e4e06f479c5b0c52cb870fd5232c9dda9a
otaviopetry <otaviopetry@gmail.com>
2021-01-17 14:09:25 -0300

    chore: Implement fonts

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,17 @@ document.querySelector('.contact-form button[type="submit"]')
     .addEventListener('click', (event) => {
         event.preventDefault();
     });
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+    })
+})
+
+document.querySelector('.contact-form form input[type="email"]').addEventListener('focus', () => {
+    document.querySelector('.contact-form form').classList.add('active');
+})
+
+document.querySelector('.contact-form form input[type="email"]').addEventListener('focusout', () => {
+    document.querySelector('.contact-form form').classList.remove('active');
+})
