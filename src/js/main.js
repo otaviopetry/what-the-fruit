f625b4a63b107827c14a4faae21da6c1e92e9d34
otaviopetry <otaviopetry@gmail.com>
2021-01-18 05:07:16 -0300

    feat: Responsive header navigation

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,13 @@ document.querySelector('.contact-form form input[type="email"]').addEventListene
 document.querySelector('.contact-form form input[type="email"]').addEventListener('focusout', () => {
     document.querySelector('.contact-form form').classList.remove('active');
 })
+
+
+const navbarToggler = document.querySelector('.site-header .navbar-toggler');
+const mainNavigation = document.querySelector('.site-header__navigation');
+const quickLinks = document.querySelector('.site-header .quick-links');
+
+navbarToggler.addEventListener('click', () => {
+    mainNavigation.classList.toggle('expanded');
+    quickLinks.classList.toggle('expanded');
+})
